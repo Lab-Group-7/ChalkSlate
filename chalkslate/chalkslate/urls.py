7b2573551f66506019cc6a2338a015ec5af32350
KowshiqueRoy <kowshiqueroy@gmail.com>
2019-12-18 12:36:12 +0600

    temp, view, urls, 28 pages

--- chalkslate/chalkslate/urls.py
+++ chalkslate/chalkslate/urls.py
@@ -1,7 +1,7 @@
 """chalkslate URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,45 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('chalkslate', include('management.urls')),
     path('admin/', admin.site.urls),
+    path('', views.first_func), #firstPage
+
+    #menu
+    path('home/', views.home),
+    path('notice-board/', views.notice_board),
+    path('guide/', views.guide),
+    path('about-us/', views.about_us),
+    path('contact-us/', views.contact_us),
+    path('signin/', views.signin),
+    path('register/', views.register),
+
+    path('reg-admin/', views.reg_admin),
+    path('reg-tutor/', views.reg_tutor),
+    path('reg-student/', views.reg_student),
+
+    path('home-admin/', views.home_admin),
+    path('home-tutor/', views.home_tutor),
+    path('home-student/', views.home_student),
+
+    path('home-admin-notice/', views.home_admin_notice),
+    path('home-admin-class/', views.home_admin_class),
+    path('home-admin-tutor/', views.home_admin_tutor),
+    path('home-admin-result/', views.home_admin_result),
+    path('home-admin-other/', views.home_admin_other),
+
+    path('home-tutor-notice/', views.home_tutor_notice),
+    path('home-tutor-class/', views.home_tutor_class),
+    path('home-tutor-attendance/', views.home_tutor_attendance),
+    path('home-tutor-result/', views.home_tutor_result),
+    path('home-tutor-other/', views.home_tutor_other),
+
+    path('home-student-notice/', views.home_student_notice),
+    path('home-student-feedback/', views.home_student_feedback),
+    path('home-student-result/', views.home_student_result),
+    path('home-student-other/', views.home_student_other),
 ]
+
