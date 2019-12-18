22ed705062ace9b69731d365e5e8eb1b48f53815
Tauhid-UAP <41752583+Tauhid-UAP@users.noreply.github.com>
2019-12-18 19:09:39 +0600

    urls reconfigured.

--- chalkslate/chalkslate/settings.py
+++ chalkslate/chalkslate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'management.apps.ManagementConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'chalkslate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- chalkslate/chalkslate/urls.py
+++ chalkslate/chalkslate/urls.py
@@ -14,45 +14,44 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.first_func), #firstPage
+    path('chalkslate/', include('management.urls')),#firstPage
 
     #menu
-    path('home/', views.home),
-    path('notice-board/', views.notice_board),
-    path('guide/', views.guide),
-    path('about-us/', views.about_us),
-    path('contact-us/', views.contact_us),
-    path('signin/', views.signin),
-    path('register/', views.register),
-
-    path('reg-admin/', views.reg_admin),
-    path('reg-tutor/', views.reg_tutor),
-    path('reg-student/', views.reg_student),
-
-    path('home-admin/', views.home_admin),
-    path('home-tutor/', views.home_tutor),
-    path('home-student/', views.home_student),
-
-    path('home-admin-notice/', views.home_admin_notice),
-    path('home-admin-class/', views.home_admin_class),
-    path('home-admin-tutor/', views.home_admin_tutor),
-    path('home-admin-result/', views.home_admin_result),
-    path('home-admin-other/', views.home_admin_other),
-
-    path('home-tutor-notice/', views.home_tutor_notice),
-    path('home-tutor-class/', views.home_tutor_class),
-    path('home-tutor-attendance/', views.home_tutor_attendance),
-    path('home-tutor-result/', views.home_tutor_result),
-    path('home-tutor-other/', views.home_tutor_other),
-
-    path('home-student-notice/', views.home_student_notice),
-    path('home-student-feedback/', views.home_student_feedback),
-    path('home-student-result/', views.home_student_result),
-    path('home-student-other/', views.home_student_other),
+    # path('home/', views.home),
+    # path('notice-board/', views.notice_board),
+    # path('guide/', views.guide),
+    # path('about-us/', views.about_us),
+    # path('contact-us/', views.contact_us),
+    # path('signin/', views.signin),
+    # path('register/', views.register),
+    #
+    # path('reg-admin/', views.reg_admin),
+    # path('reg-tutor/', views.reg_tutor),
+    # path('reg-student/', views.reg_student),
+    #
+    # path('home-admin/', views.home_admin),
+    # path('home-tutor/', views.home_tutor),
+    # path('home-student/', views.home_student),
+    #
+    # path('home-admin-notice/', views.home_admin_notice),
+    # path('home-admin-class/', views.home_admin_class),
+    # path('home-admin-tutor/', views.home_admin_tutor),
+    # path('home-admin-result/', views.home_admin_result),
+    # path('home-admin-other/', views.home_admin_other),
+    #
+    # path('home-tutor-notice/', views.home_tutor_notice),
+    # path('home-tutor-class/', views.home_tutor_class),
+    # path('home-tutor-attendance/', views.home_tutor_attendance),
+    # path('home-tutor-result/', views.home_tutor_result),
+    # path('home-tutor-other/', views.home_tutor_other),
+    #
+    # path('home-student-notice/', views.home_student_notice),
+    # path('home-student-feedback/', views.home_student_feedback),
+    # path('home-student-result/', views.home_student_result),
+    # path('home-student-other/', views.home_student_other),
 ]
 
