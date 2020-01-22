d296e4fc20485d0fe3c2076ac014ea3884b4dbfc
KowshiqueRoy <kowshiqueroy@gmail.com>
2020-01-22 11:26:49 +0600

    Update models.py

--- chalkslate/management/models.py
+++ chalkslate/management/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Institute(models.Model):
+
+    institute_username = models.Char(max_length = 200, default = '')
+    institute_name = models.Char(max_length = 200, default = '')
+    institute_details = models.Char(max_length = 200, default = '')
+
+class Student(models.Model):
+
+    
