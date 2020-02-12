4f7fd596dd71664eae8234163dc41322dde75940
uchsas17 <davidmason597@gmail.com>
2020-02-13 02:37:13 +0600

    design 2 by kush

--- chalkslate/chalkslate/urls.py
+++ chalkslate/chalkslate/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+    path('',views.start),
+
 
     path('chalkslate/', include('management.urls')),
 
